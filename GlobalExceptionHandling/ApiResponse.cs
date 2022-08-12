da1414025468742910b593f26e98123628e67776
Moataz Allam <moataz.allam@nt-me.com>
2022-08-13 01:38:25 +0200

    Pushing entire project

---
+++ GlobalExceptionHandling/ApiResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3
+{
+    public class ApiResponse<T>
+    {
+        public T Data { get; set; }
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+        public static ApiResponse<T> Fail(string errorMsg)
+        {
+            return new ApiResponse<T> { Succeeded = false, Message = errorMsg };
+        }
+        public static ApiResponse<T> Success(T data)
+        {
+            return new ApiResponse<T> { Succeeded = true, Data = data };
+        }
+    }
+}
